9ec9a13ff38ad1f5adeaf6211a201237d5ead256
SchwJ <67754964+SchwJ@users.noreply.github.com>
2023-04-09 21:27:10 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,10 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 5191217616 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+def solution(x: np.array, y: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    return anderson_ksamp([x, y]).pvalue < 0.01 # Ваш ответ, True или False
