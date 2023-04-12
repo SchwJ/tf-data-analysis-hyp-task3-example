3889b8b499743eaaeb2bbc134c97a64342042f71
SchwJ <67754964+SchwJ@users.noreply.github.com>
2023-04-13 01:21:11 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -3,10 +3,11 @@ import numpy as np
 from scipy.stats import anderson_ksamp
 
 
-chat_id = 5191217616 # Ваш chat ID, не меняйте название переменной
+chat_id = 783077646 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return anderson_ksamp([x, y]).pvalue < 0.01 # Ваш ответ, True или False
+    _, res = w.ztest(x, value=500, alternative='larger')
+    return res <= 0.1 # Ваш ответ, True или False
